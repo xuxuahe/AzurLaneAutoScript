8f0bc80d98cbff01732d5c7b2059c76e6b0b9d77
guoh064 <50830808+guoh064@users.noreply.github.com>
2025-12-10 14:17:19 +0800

    Feature: dashboard

--- module/raid/raid.py
+++ module/raid/raid.py
@@ -12,6 +12,9 @@ from module.map.map_operation import MapOperation
 from module.ocr.ocr import Digit, DigitCounter
 from module.raid.assets import *
 from module.raid.combat import RaidCombat
+from module.ui.assets import RAID_CHECK
+from module.ui.page import page_rpg_stage
+from module.log_res import LogRes
 from module.ui.page import page_raid, page_rpg_stage
 
 
@@ -425,10 +428,12 @@ class Raid(MapOperation, RaidCombat, CampaignEvent):
                 pt = ocr.ocr(self.device.image)
                 if timeout.reached():
                     logger.warning('Wait PT timeout, assume it is')
+                    LogRes(self.config).Pt = pt
                     return pt
                 if pt in [70000, 70001]:
                     continue
                 else:
+                    LogRes(self.config).Pt = pt
                     return pt
         else:
             logger.info(f'Raid {self.config.Campaign_Event} does not support PT ocr, skip')
