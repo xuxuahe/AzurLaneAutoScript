0b1bb7c7c413f5349b353f2982280446fb1a23f5
baizi826 <1131546071@qq.com>
2026-03-04 16:45:24 +0800

    Personal-customization

---
+++ run_alas-mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 将终端窗口最小化
+osascript -e 'tell application "Terminal" to set miniaturized of front window to true'
+
+# 初始化 Conda
+eval "$(conda shell.bash hook)"
+
+# 激活 alas 环境
+conda activate alas
+
+# 切换到 alas 目录
+cd AzurLaneAutoScript
+
+# 延迟1秒自动访问http://127.0.0.1:22267
+(sleep 1 && open http://127.0.0.1:22267) &
+
+# 运行 gui.py
+python gui.py
